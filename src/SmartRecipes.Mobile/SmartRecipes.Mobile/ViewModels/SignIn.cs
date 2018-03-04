aef88d563f4e2e861126ab767afd6c4fc362a887
Josef Starychfojtu <josef.starychfojtu@gmail.com>
2018-03-04 22:32:33 +0100

    Viewmodels init

---
+++ src/SmartRecipes.Mobile/SmartRecipes.Mobile/ViewModels/SignIn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SmartRecipes.Mobile
+{
+    public class SignIn
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public SignIn()
+        {
+        }
+    }
+}
