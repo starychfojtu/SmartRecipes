8279d69321cfb47413b3b3793f9a91451dee95b5
Josef Starychfojtu <josef.starychfojtu@gmail.com>
2018-04-02 19:14:38 +0200

    Recipe cell base

--- src/SmartRecipes.Mobile/SmartRecipes.Mobile/Views/RecipeCell.xaml.cs
+++ src/SmartRecipes.Mobile/SmartRecipes.Mobile/Views/RecipeCell.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using Xamarin.Forms;
+using FFImageLoading.Transformations;
+using System.Linq;
+using FFImageLoading.Forms;
 
 namespace SmartRecipes.Mobile.Views
 {
@@ -31,8 +34,22 @@ namespace SmartRecipes.Mobile.Views
             if (ViewModel != null)
             {
                 NameLabel.Text = ViewModel.Recipe.Name;
-
                 OtherButton.IsVisible = ViewModel.OnOther != null;
+                IngredientsStackLayout.Children.Clear();
+
+                foreach (var ingredient in ViewModel.Recipe.Ingredients)
+                {
+                    var image = new CachedImage
+                    {
+                        HeightRequest = 32,
+                        WidthRequest = 32,
+                        VerticalOptions = LayoutOptions.Center,
+                        Source = ingredient.Foodstuff.ImageUrl,
+                        DownsampleToViewSize = true
+                    };
+                    image.Transformations.Add(new CircleTransformation());
+                    IngredientsStackLayout.Children.Add(image);
+                }
             }
         }
     }
