ff9ce0dcddad659a70b43b449985666744d01312
Josef Starychfojtu <josef.starychfojtu@gmail.com>
2018-07-11 20:04:27 +0200

    Sign up fix

--- src/SmartRecipes.UseCases/Users.fs
+++ src/SmartRecipes.UseCases/Users.fs
@@ -7,12 +7,17 @@ module UseCases.Users
     open FSharpPlus.Data
     
     // TODO: move this to data access
+    let toDbModel (account: Models.User.Account): Database.Model.Account = {
+        id = match account.id with | AccountId id -> id
+        email = account.credentials.email.Address
+        password = match account.credentials.password with | Password p -> p
+    }
+    
     let toModel (dbAccount: Database.Model.Account): Models.User.Account option =
         match mkCredentials dbAccount.email dbAccount.password with 
         | Success c -> Some { id = AccountId dbAccount.id; credentials = c }
         | Failure _ -> None
         
-    // TODO: move this to data access
     let getUserByEmail (context: Context) (email: MailAddress) =
         context.Accounts 
         |> Seq.filter (fun a -> a.email = email.Address)
@@ -25,7 +30,7 @@ module UseCases.Users
         let result = Users.signUp email password <| getUserByEmail context
         match result with
         | Ok a ->
-            context.Add(a) |> ignore
+            context.Add(toDbModel a) |> ignore
             context.SaveChanges() |> ignore
             Ok a
         | Error e -> Error e  
