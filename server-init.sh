83d2031a91a91c3caf5a45f99fa33bc2e5cd628e
Josef Starychfojtu <josef.starychfojtu@gmail.com>
2018-09-09 01:50:19 +0200

    test

--- server-init.sh
+++ server-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo dotnet publish -c Release -o /var/smart-recipes/publish
+sudo dotnet publish -c Release -o /var/smart-recipes/publish /var/smart-recipes/src/SmartRecipes.Api/src/SmartRecipes.Api/SmartRecipes.Api.fsproj
 sudo dotnet /var/smart-recipes/publish/SmartRecipes.Api.dll
 sudo service httpd restart > /var/log/restartapache.out 2>&1
 
