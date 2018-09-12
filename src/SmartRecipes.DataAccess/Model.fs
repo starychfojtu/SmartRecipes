93e5fb75435973f1f631d043f3784d23f519a756
Josef Starychfojtu <josef.starychfojtu@gmail.com>
2018-09-12 21:29:17 +0200

    MongoRepository removed

--- src/SmartRecipes.DataAccess/Model.fs
+++ src/SmartRecipes.DataAccess/Model.fs
@@ -1,39 +1,64 @@
 module DataAccess.Model
     open System.Collections.Generic
     open System
-    open MongoRepository
     
-    type DbAccount(id: Guid, email: string, password: string) =
-        inherit Entity()
-        member this.Id = id
-        member this.Email = email
-        member this.Password = password
+    type DbAccount = {
+        id: Guid
+        email: string
+        password: string
+    }
 
-    type DbAccessToken(accountId: Guid, value: string, expiration: DateTime) =
-        inherit Entity()
+    type DbAccessToken = {
+        accountId: Guid
+        value: string
+        expiration: DateTime
+    }
     
     type DbMetricUnit = 
         | Liter = 0
         | Gram = 1
         | Piece = 2
         
-    type DbAmount(unit: DbMetricUnit, value: float) =
-        inherit obj()
+    type DbAmount = {
+        unit: DbMetricUnit
+        value: float
+    }
 
-    type DbFoodstuff(id: Guid, name: string, baseAmount: DbAmount, amountStep: DbAmount) =
-        inherit Entity()
+    type DbFoodstuff = {
+        id: Guid
+        name: string
+        baseAmount: DbAmount
+        amountStep: DbAmount
+    }
     
-    type DbIngredient(foodstuffId: Guid, amount: float) =
-        inherit obj()
-
-    type DbRecipe(id: Guid, name: string, creatorId: Guid, personCount: int, imageUrl: string, description: string, ingredients: seq<DbIngredient>) = 
-        inherit Entity()
-        
-    type DbListItem(foodstuffId: Guid, amount: float) = 
-        inherit obj()
-        
-    type DbRecipeListItem(recipeId: Guid, personCount: int) = 
-        inherit obj()
-        
-    type DbShoppingList(id: Guid, accountId: Guid, items: seq<DbListItem>, recipes: seq<DbRecipeListItem>) =
-        inherit Entity()
+    type DbIngredient = {
+        foodstuffId: Guid
+        amount: float
+    }
+    
+    type DbRecipe = {
+        id: Guid
+        name: string
+        creatorId: Guid
+        personCount: int
+        imageUrl: string
+        description: string
+        ingredients: seq<DbIngredient>
+    }
+    
+    type DbListItem = {
+        foodstuffId: Guid
+        amount: float
+    }
+    
+    type DbRecipeListItem = {
+        recipeId: Guid
+        personCount: int
+    }
+    
+    type DbShoppingList = {
+        id: Guid
+        accountId: Guid
+        items: seq<DbListItem>
+        recipes: seq<DbRecipeListItem>
+    }
--- src/SmartRecipes.DataAccess/Users.fs
+++ src/SmartRecipes.DataAccess/Users.fs
@@ -5,7 +5,6 @@ module DataAccess.Users
     open Domain.Password
     open FSharpPlus.Data
     open Domain.Token
-    open MongoRepository
     
     type UsersDao = {
         getById: AccountId -> Account option
@@ -13,41 +12,38 @@ module DataAccess.Users
         add: Account -> Account
     }
     
-    let private toDb account = new DbAccount(
-        match account.id with AccountId id -> id,
-        account.credentials.email.Address,
-        match account.credentials.password with Password p -> p
-    )
-    
-    let private toModel (dbAccount: DbAccount): Account = {
-        id = AccountId dbAccount.Id
-        credentials = 
-        {
-            email = new MailAddress(dbAccount.Email)
-            password = Password dbAccount.Password
-        }
+    let private toDb account: DbAccount = {
+        id = match account.id with AccountId id -> id
+        email = account.credentials.email.Address
+        password = match account.credentials.password with Password p -> p
     }
     
-    let private repository = new MongoRepository<DbAccount>()
-        
+    let private toModel (dbAccount: DbAccount): Account = {
+        id = AccountId dbAccount.id
+        credentials = 
+        {
+            email = new MailAddress(dbAccount.email)
+            password = Password dbAccount.password
+        }
+    }
+
     let private add account =
-        repository.Add (toDb account) |> ignore
         account
         
-    let private getByEmail (email: MailAddress) =
-        repository
-        |> Seq.filter (fun a -> a.Email = email.Address)
-        |> Seq.tryHead
-        |> Option.map toModel
-        
-    let private getById (AccountId id) =
-        repository
-        |> Seq.filter (fun a -> a.Id = id)
-        |> Seq.tryHead
-        |> Option.map toModel
+//    let private getByEmail (email: MailAddress) =
+//        repository
+//        |> Seq.filter (fun a -> a.Email = email.Address)
+//        |> Seq.tryHead
+//        |> Option.map toModel
+//        
+//    let private getById (AccountId id) =
+//        repository
+//        |> Seq.filter (fun a -> a.Id = id)
+//        |> Seq.tryHead
+//        |> Option.map toModel
             
     let getDao () = {
-        getById = getById
-        getByEmail = getByEmail
+        getById = fun id -> None
+        getByEmail = fun email -> None
         add = add
     }
