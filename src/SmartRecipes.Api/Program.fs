7a3f2ae52c12957e418338d20607f2dd51221fba
Josef Starychfojtu <josef.starychfojtu@gmail.com>
2018-09-25 23:22:11 +0200

    Database credentials moved to env variables

--- src/SmartRecipes.Api/Program.fs
+++ src/SmartRecipes.Api/Program.fs
@@ -12,6 +12,7 @@ module SmartRecipes.Api.App
     open Microsoft.Extensions.DependencyInjection
     open Microsoft.AspNetCore.Http
     open Microsoft.AspNetCore.HttpOverrides
+    open MongoDB.Driver
     
     let webApp =
         choose [
@@ -51,8 +52,23 @@ module SmartRecipes.Api.App
     // Config and Main
     // ---------------------------------
     
+    let configureDatabase isProd =
+        let connStr = 
+            if isProd
+            then
+                let vars = System.Environment.GetEnvironmentVariables ();
+                let username = string(vars.["DATABASE_USERNAME"]);
+                let password = string(vars.["DATABASE_PASSWORD"]);
+                sprintf "mongodb://%s:%s@ds213053.mlab.com:13053/heroku_st1qt292" username password
+            else 
+                "mongodb://localhost"
+        let databaseName = if isProd then "heroku_st1qt292" else "SmartRecipes"
+        let database = MongoClient(connStr).GetDatabase(databaseName)
+        DataAccess.Database.database <- database // TODO: I am ugly, refactor me
+    
     let configureApp (app : IApplicationBuilder) =
         let env = app.ApplicationServices.GetService<IHostingEnvironment>()
+        configureDatabase (env.IsProduction()) |> ignore
         (match env.IsDevelopment() with
         | true  -> app.UseDeveloperExceptionPage()
         | false -> app.UseGiraffeErrorHandler errorHandler)
@@ -61,7 +77,7 @@ module SmartRecipes.Api.App
     
     let configureServices (services : IServiceCollection) =
         services
-            .AddGiraffe() 
+            .AddGiraffe()
             |> ignore
     
     let configureLogging (builder : ILoggingBuilder) =
--- src/SmartRecipes.DataAccess/Database.fs
+++ src/SmartRecipes.DataAccess/Database.fs
@@ -2,16 +2,8 @@ module DataAccess.Database
     open MongoDB.Driver
     open MongoDB.FSharp
     
-    let mutable private database: IMongoDatabase = null
-    
-    let private initialize () =
-        let prodConnectionString = "mongodb://prod:smartrecipes45@ds213053.mlab.com:13053/heroku_st1qt292"
-        // let localConnectionString = "mongodb://localhost"
-        let client = new MongoClient(prodConnectionString)
-        // client.GetDatabase("SmartRecipes")
-        client.GetDatabase("heroku_st1qt292")
+    let mutable database: IMongoDatabase = null
         
     let getCollection<'a> () =
-        let database = initialize ()
         database.GetCollection<'a> typeof<'a>.Name
         
