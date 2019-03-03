8f7eedc58d60eb4e4885107894a943fc79998954
Josef Starýchfojtů <josef.starychfojtu@gmail.com>
2019-03-03 18:28:03 +0100

    Delete server-init.sh

--- server-init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo dotnet publish -c Release -o /var/smart-recipes/publish /var/smart-recipes/src/SmartRecipes.Api/src/SmartRecipes.Api/SmartRecipes.Api.fsproj
-#sudo dotnet /var/smart-recipes/publish/SmartRecipes.Api.dll
-#sudo service httpd restart > /var/log/restartapache.out 2>&1
-
